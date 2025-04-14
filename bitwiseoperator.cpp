e4a8c9e89a0cc05c8488136ff381c4f6991b0ad4
Musab <joiya4636@gmail.com>
2025-04-14 01:45:46 -0700

    Create bitwiseoperator.cpp

---
+++ bitwiseoperator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 4;
+    int b = 6;
+
+    cout << "a & b = " << (a & b) << endl;  // Bitwise AND
+    cout << "a | b = " << (a | b) << endl;  // Bitwise OR
+    cout << "!a = " << (!a) << endl;        // Logical NOT
+    cout << "a ^ b = " << (a ^ b) << endl;  // Bitwise XOR
+
+    return 0;
+}

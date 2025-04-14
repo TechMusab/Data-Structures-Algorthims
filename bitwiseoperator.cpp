db3aa0770672dbeebc4fc2ccec995b58bca091f7
Musab <joiya4636@gmail.com>
2025-04-14 02:02:14 -0700

    Update bitwiseoperator.cpp

--- bitwiseoperator.cpp
+++ bitwiseoperator.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int a = 4;
     int b = 6;
 
-    cout << "a & b = " << (a & b) << endl;  // Bitwise AND
-    cout << "a | b = " << (a | b) << endl;  // Bitwise OR
-    cout << "!a = " << (!a) << endl;        // Logical NOT
-    cout << "a ^ b = " << (a ^ b) << endl;  // Bitwise XOR
-
+    cout << "a & b = " << (a & b) << endl; // Bitwise AND
+    cout << "a | b = " << (a | b) << endl; // Bitwise OR
+    cout << "!a = " << (!a) << endl;       // Logical NOT
+    cout << "a ^ b = " << (a ^ b) << endl; // Bitwise XOR
+    cout << (17 << 2) <<endl; //left shift
+    cout<< (17>>2) <<endl;  //right shift
     return 0;
 }

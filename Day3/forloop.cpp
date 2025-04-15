0c7f78d058f508b7e1d9b69c1576066373504846
Musab <joiya4636@gmail.com>
2025-04-15 11:14:21 -0700

    day 3

---
+++ Day3/forloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // for loop
+    for(int i=0; i<5; i++){
+        cout << "Hello World" << endl;
+    }
+    //for loop double variable
+    for(int a=0, b=0; a<5; a++, b++){
+        cout << "Hello World" << endl;
+    }
+
+    return 0;
+}

10365eecbdb4cc2de4ca48cf3ba87f174600d429
Musab <joiya4636@gmail.com>
2025-04-16 03:05:13 -0700

    updated

---
+++ Day3/continue.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // continue keyword
+    //  agar ap itertion ko skip krna chate ho
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "hello" << endl;
+        cout << "Hi" << endl;
+        continue;
+        cout << "varr gaye";
+    }
+    
+  return 0;
+}

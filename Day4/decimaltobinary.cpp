493a975b53d5c129dac0c360ea0cc1de34883e34
Musab <joiya4636@gmail.com>
2025-04-17 11:17:36 -0700

    Updated

--- Day4/decimaltobinary.cpp
+++ Day4/decimaltobinary.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
 
-int main(){
-int n;
-cout<<"enter n"<<endl;
-cin>>n;
-int ans=0;
-int i=0;
-while(n!=0){
-    int bit=n & 1;
-    ans=(bit*pow(10,i)) +ans;
-    n=n >> 1;
-    i++;
-}
-cout<<ans;
+int main()
+{
+    int n;
+    cout << "enter n" << endl;
+    cin >> n;
+    int ans = 0;
+    int i = 0;
+    while (n != 0)
+    {
+        int bit = n & 1;
+        ans = (bit * pow(10, i)) + ans;
+        n = n >> 1;
+        i++;
+    }
+    cout << ans;
     return 0;
 }

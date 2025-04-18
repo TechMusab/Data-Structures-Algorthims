f0bc6b0a733750a21256751297c8e6870a080b85
Musab <joiya4636@gmail.com>
2025-04-18 13:25:31 -0700

    Problem 1 Reverse Integer

--- Day5/leetcodeproblems.cpp
+++ Day5/leetcodeproblems.cpp
@@ -1,7 +1,27 @@
-#include<iostream>
+// Problem 1: Reverse integer
+#include <iostream>
+#include <climits> 
 using namespace std;
+int reverseinteger(int i)
+{
+    int ans = 0;
+    while (i != 0)
+    {
+        //normal case
+        int digit = i % 10;
+        ans = (ans * 10) + digit;
+        //special case
+        if(ans<INT_MIN/10 || ans>INT_MAX/10){
+            return 0;
+        }
+        i = i / 10;
+    }
+    return ans;
+}
+int main()
+{
 
-int main(){
-//reverse integer
+    cout<<reverseinteger(123443);
+
     return 0;
 }

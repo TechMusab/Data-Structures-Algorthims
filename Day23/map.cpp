c29542f1fcfeed3266403b4151694dfc1ca3b171
Musab <joiya4636@gmail.com>
2025-05-10 10:15:44 -0700

    Update map.cpp

--- Day23/map.cpp
+++ Day23/map.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <map>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -20,5 +22,24 @@ int main()
     cout << m.count("Hello") << endl;
     m.erase("Hello");
     cout << m.count("Hello") << endl;
+
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    cout << "Binary search" << endl;
+    cout << binary_search(v.begin(), v.end(), 3) << endl;
+    cout<<upper_bound(v.begin(), v.end(), 3) - v.begin() << endl;
+    cout<<lower_bound(v.begin(), v.end(), 5) - v.begin() <<endl;
+    int a=5;
+    int b=2;
+    cout<<max(a,b)<<endl;
+    cout<<min(a,b)<<endl;
+
+    string s="Hello";
+    reverse(s.begin(),s.end());
+    cout<<s;
     return 0;
 }

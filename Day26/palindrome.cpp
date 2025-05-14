9d004d92f332da6639feec399f03b598ebc20094
Musab <joiya4636@gmail.com>
2025-05-13 18:40:46 -0700

    Update palindrome.cpp

--- Day26/palindrome.cpp
+++ Day26/palindrome.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
 using namespace std;
+
+char toLowercase(char ch)
+{
+    if (ch >= 'a' && ch <= 'z')
+    {
+        return ch;
+    }
+    else
+    {
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
 bool isPalindrome(char str[], int n)
 {
     int s = 0;
     int e = n - 1;
     while (s <= e)
     {
-        if (str[s] != str[e])
+        // ignoring special charaters spaces
+        if (!((str[s] >= 'a' && str[s] <= 'z') || (str[s] >= 'A' && str[s] <= 'Z')))
+        {
+            s++;
+        }
+        else if (!((str[e] >= 'a' && str[e] <= 'z') || (str[e] >= 'A' && str[e] <= 'Z')))
+        {
+            e--;
+        }
+        else if (toLowercase(str[s]) != toLowercase(str[e]))
         {
             return false;
         }
-        s++;
-        e--;
+        else
+        {
+
+            s++;
+            e--;
+        }
     }
     return true;
 }
 
 int main()
 {
-    char ch[10];
-    cout << "Enter a string: ";
-    cin >> ch;
+    char ch[100]="n$%a@^n";
     int i = 0;
     while (ch[i] != '\0')
     {
@@ -30,7 +54,8 @@ int main()
     {
         cout << "The string is a palindrome." << endl;
     }
-    else{
+    else
+    {
         cout << "The string is not a palindrome." << endl;
     }
     return 0;

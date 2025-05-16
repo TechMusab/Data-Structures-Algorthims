b152f38dad340370b51868469bc4d969496ee282
Musab <joiya4636@gmail.com>
2025-05-16 00:28:51 -0700

    Create removeduplicates.cpp

---
+++ Day28/removeduplicates.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<stack>
+using namespace std;
+string removeDuplicates(string s)
+{
+    stack<char> s1;
+    for (char c : s)
+    {
+        if (!s1.empty() && s1.top() == c)
+        {
+            s1.pop();
+        }
+        else
+        {
+            s1.push(c);
+        }
+    }
+    string result = "";
+    while (!s1.empty())
+    {
+        result += s1.top();
+        s1.pop();
+    }
+
+    reverse(result.begin(), result.end());
+
+    return result;
+}
+
+int main()
+{
+
+    return 0;
+}

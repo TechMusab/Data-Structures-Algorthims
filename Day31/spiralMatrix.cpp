92488ed271169382163531c437aeb1a1ede3f31a
Musab <joiya4636@gmail.com>
2025-05-19 11:48:02 -0700

    Update spiralMatrix.cpp

--- Day31/spiralMatrix.cpp
+++ Day31/spiralMatrix.cpp
@@ -1,7 +1,56 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+    int row = matrix.size();
+    int cols = matrix[0].size();
+
+    int total = row * cols;
+    int count = 0;
+    int startingRow = 0;
+    int endingRow = row - 1;
+    int startingCol = 0;
+    int endingCol = cols - 1;
+    vector<int> ans;
+
+    while (count < total)
+    {
+        // print starting row
+        for (int i = startingCol; count < total && i <= endingCol; i++)
+        {
+            ans.push_back(matrix[startingRow][i]);
+            count++;
+        }
+        startingRow++;
+        // print ending column
+        for (int i = startingRow; count < total && i <= endingRow; i++)
+        {
+            ans.push_back(matrix[i][endingCol]);
+            count++;
+        }
+        endingCol--;
+        // print ending row
+        for (int i = endingCol; count < total && i >= startingCol; i--)
+        {
+            ans.push_back(matrix[endingRow][i]);
+            count++;
+        }
+        endingRow--;
+        // print starting column
+        for (int i = endingRow; count < total && i >= startingRow; i--)
+        {
+            ans.push_back(matrix[i][startingCol]);
+            count++;
+        }
+        startingCol++;
+    }
+    return ans;
+}
+
+int main()
+{
 
     return 0;
 }

117ceda0a3f8df48683263f2e0ebbff4dd33bcc6
Musab <joiya4636@gmail.com>
2025-05-21 12:05:15 -0700

    Update Day33..cpp

--- Day33/Day33..cpp
+++ Day33/Day33..cpp
@@ -1,6 +1,24 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int rowIndex = 0;
+        int colIndex = cols - 1;
+        while (rowIndex < rows && colIndex >= 0) {
+            int element = matrix[rowIndex][colIndex];
+            if (element == target) {
+                return 1;
+            } else if (element > target) {
+                colIndex--;
 
+            } else {
+                rowIndex++;
+            }
+        }
+        return 0;
+    }
 int main(){
 
     return 0;

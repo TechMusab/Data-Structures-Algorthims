cef0a08cb31618a64118c3803d2eb65809418713
Musab <joiya4636@gmail.com>
2025-05-23 12:30:13 -0700

    Update Day34.cpp

--- Day34/Day34.cpp
+++ Day34/Day34.cpp
@@ -1,7 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int countPrimes(int n)
+{
+    int count = 0;
+    for (int i = 2; i < n; i++)
+    {
+        if (isPrime(i))
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
-int main(){
-
-
+int main()
+{
 }

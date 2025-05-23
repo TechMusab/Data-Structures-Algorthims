81cf0c70f6d90cdbb8aae5b43ecd767b2787afd0
Musab <joiya4636@gmail.com>
2025-05-23 12:42:40 -0700

    Update Day34.cpp

--- Day34/Day34.cpp
+++ Day34/Day34.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 bool isPrime(int n)
 {
@@ -23,6 +24,27 @@ int countPrimes(int n)
         if (isPrime(i))
         {
             count++;
+        }
+    }
+    return count;
+}
+// optimized approach
+int countPrimes(int n)
+{
+    int count = 0;
+    vector<bool> prime(n + 1, true);
+    prime[0] = prime[1] = false;
+    for (int i = 2; i < n; i++)
+    {
+        if (prime[i])
+        {
+            count++;
+            int j = 2;
+            while (j * i < n)
+            {
+                prime[j * i] = false;
+                j += 1;
+            }
         }
     }
     return count;

580f837e66564d264bdcd05bd172425d5e174f58
Musab <joiya4636@gmail.com>
2025-06-24 15:02:51 -0700

    Update introtopointers.cpp

--- Day37/introtopointers.cpp
+++ Day37/introtopointers.cpp
@@ -4,8 +4,16 @@ using namespace std;
 int main(){
 int num=5;
 int *ptr=&num; 
-cout<<ptr<<endl; // Address of num
-cout<<*ptr<<endl; // Value at the address stored in ptr
+cout<<ptr<<endl; 
+cout<<*ptr<<endl; 
+cout<<"Before:"<<num<<endl;
+(*ptr)++;
+cout<<"After:"<<num<<endl; 
+int *q=ptr;
+//Copying a pointer
+cout<<q<<"-"<<ptr<<endl;
+cout<<*q<<"-"<<*ptr<<endl;
+
 
 
     return 0;

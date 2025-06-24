dc1bf578e12f3a1ed96c822b9c24985cbe19fdf7
Musab <joiya4636@gmail.com>
2025-06-24 15:07:54 -0700

    Update introtopointers.cpp

--- Day37/introtopointers.cpp
+++ Day37/introtopointers.cpp
@@ -6,14 +6,23 @@ int num=5;
 int *ptr=&num; 
 cout<<ptr<<endl; 
 cout<<*ptr<<endl; 
+
 cout<<"Before:"<<num<<endl;
 (*ptr)++;
 cout<<"After:"<<num<<endl; 
+
 int *q=ptr;
-//Copying a pointer
 cout<<q<<"-"<<ptr<<endl;
 cout<<*q<<"-"<<*ptr<<endl;
 
+int i=3;
+int *j=&i;
+*j=*j+1;
+cout<<*j<<endl;
+
+cout<<"Before:"<<j<<endl;
+j=j+1; 
+cout<<"After:"<<j<<endl;
 
 
     return 0;
